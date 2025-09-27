b5322dea2d6b1e8e8ee93500658d6a8bbb4556d8
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2025-09-27 22:53:27 +0600

    add a web page with static image file

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.say_hello, name='say-hello'),
+    path('', views.index, name='index'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def say_hello(request):
     return HttpResponse("Hello from Django!")
+
+
+def index(request):
+    return render(request, 'index.html', {})
+
